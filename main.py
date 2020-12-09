2d37bd8276ab9c70059704c00532f5f1492c2854
Dainokode <antoniodainok@gmail.com>
2020-12-09 12:28:54 +0100

    more refactor

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-# # drinks available
-# espresso = MenuItem("espresso", 50, 0, 18, 1.5)
-# latte = MenuItem("latte", 200, 150, 18, 2.5)
-# cappuccino = MenuItem("cappuccino", 300, 200, 18, 3.5)
 
 menu = Menu()
 coffee_maker = CoffeeMaker()
